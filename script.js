69fa6ed3ca228178b6e4aa07dbeaed082f75abe5
tathanz98 <119460323+tathanz98@users.noreply.github.com>
2024-02-24 23:57:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
+
+function descargarCV() {
+  // Crear un elemento <a> temporal
+  var enlace = document.createElement('a');
+  // Establecer la URL del archivo CV
+  enlace.href = 'index.html'; // Reemplaza con la ruta de tu CV
+  // Establecer el atributo download para que se descargue en lugar de abrirse
+  enlace.download = 'mi_porta_folio_cv'; // Reemplaza con el nombre que desees para el archivo
+  // Hacer clic en el enlace para iniciar la descarga
+  enlace.click();
+}
